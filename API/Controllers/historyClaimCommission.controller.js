d27009eb7bac9091474c87566cb2a1138d200bb6
ThanhNga29 <103370636+ThanhNga29@users.noreply.github.com>
2024-03-27 16:10:46 +0700

    idCommission, getCommissionId, getAllCommissionHistory

--- API/Controllers/historyClaimCommission.controller.js
+++ API/Controllers/historyClaimCommission.controller.js
@@ -1,3 +1,4 @@
+const historyClaimRoseModel = require("../../Models/historyClaimRose.model");
 const claimCommisionModel = require("../../Models/historyClaimRose.model");
 const userModel = require("../../Models/user.model");
 const claimCommissionController = {
@@ -22,6 +23,77 @@ const claimCommissionController = {
       return res.status(200).json({
         sucess: true,
         data: saveClaimCommission,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        sucess: false,
+        message: error.message,
+      });
+    }
+  },
+  getAllCommission: async (req, res, next) => {
+    try {
+      const findAllCommission = await claimCommisionModel
+        .find()
+        .populate({ path: "addressWallet" });
+      return res.status(200).json({
+        sucess: true,
+        data: findAllCommission,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        sucess: false,
+        message: error.message,
+      });
+    }
+  },
+  getIdCommission: async (req, res, next) => {
+    try {
+      const findIdCommission = await claimCommisionModel.findOne({
+        _id: req.params._id,
+      });
+      if (!findIdCommission) {
+        return res.status(404).json({
+          sucess: false,
+          message: "The id history commission not found!",
+        });
+      }
+      return res.status(200).json({
+        sucess: true,
+        data: findIdCommission,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        sucess: false,
+        message: error.message,
+      });
+    }
+  },
+  updateHistoryCommission: async (req, res, next) => {
+    try {
+      const findHistoryCommission = await historyClaimRoseModel.findOne({
+        _id: req.params._id,
+      });
+      if (!findHistoryCommission) {
+        return res.status(404).json({
+          sucess: false,
+          message: "The Id history claim rose not found!",
+        });
+      }
+      const conditionCommission = {
+        _id: req.params._id,
+      };
+      const updatedDataCommission = {
+        state: true,
+      };
+      const updatedCommission = await rewardModel.findOneAndUpdate(
+        conditionCommission,
+        updatedDataCommission,
+        { new: true }
+      );
+      return res.status(200).json({
+        message: "Updated state successfully!",
+        data: updatedCommission,
       });
     } catch (error) {
       return res.status(500).json({
--- API/Routers/historyClaimCommission.router.js
+++ API/Routers/historyClaimCommission.router.js
@@ -6,5 +6,17 @@ historyClaimCommissionRouter.post(
   "/api/claim/commission",
   historyClaimCommissionController.claimCommission
 );
+historyClaimCommissionRouter.get(
+  "/api/all/history/commission",
+  historyClaimCommissionController.getAllCommission
+);
+historyClaimCommissionRouter.get(
+  "/api/id/commission/:_id",
+  historyClaimCommissionController.getIdCommission
+);
+historyClaimCommissionRouter.put(
+  "/api/history/claim/:_id",
+  historyClaimCommissionController.updateHistoryCommission
+);
 
 module.exports = historyClaimCommissionRouter;
