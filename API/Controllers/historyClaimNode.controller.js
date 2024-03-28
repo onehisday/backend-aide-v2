94730dafe5cc74d0c1dc9d650ffbc07250e7d0be
ThanhNga29 <103370636+ThanhNga29@users.noreply.github.com>
2024-03-28 07:48:35 +0700

    historyClaimNode

--- API/Controllers/historyClaimNode.controller.js
+++ API/Controllers/historyClaimNode.controller.js
@@ -40,6 +40,66 @@ const claimNodeController = {
       });
     }
   },
+  getAllHistoryClaimNode: async (req, res, next) => {
+    try {
+      const historyClaimNode = await historyClaimNodeModel
+        .find()
+        .populate({ path: "addressWallet" });
+      return res.status(200).json({
+        sucess: true,
+        data: historyClaimNode,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        sucess: false,
+        message: error.message,
+      });
+    }
+  },
+  getIdHistoryClaimNode: async (req, res, next) => {
+    try {
+      const findById = await historyClaimNodeModel
+        .findById({ _id: req.params._id })
+        .populate({ path: "addressWallet" });
+      if (!findById) {
+        return res.status(404).json({
+          sucess: false,
+          message: "The id history claim node not found!",
+        });
+      }
+      return res.status(200).json({
+        sucess: true,
+        data: findById,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        sucess: false,
+        message: error.message,
+      });
+    }
+  },
+  getIdWalletAddress: async (req, res, next) => {
+    try {
+      const findByWalletAddress = await historyClaimNodeModel
+        .findOne({ addressWallet: req.params._id })
+        .populate({ path: "addressWallet" });
+      if (!findByWalletAddress) {
+        return res.status(404).json({
+          sucess: false,
+          message: "The id address wallet not found!",
+        });
+      }
+      return res.status(200).json({
+        sucess: true,
+        data: findByWalletAddress,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        sucess: false,
+        message: error.message,
+      });
+    }
+  },
 };
 
 module.exports = claimNodeController;
--- API/Routers/historyClaimNode.router.js
+++ API/Routers/historyClaimNode.router.js
@@ -6,4 +6,16 @@ historyClaimNodeRouter.post(
   "/api/claim/node",
   historyClaimNodeController.claimNode
 );
+historyClaimNodeRouter.get(
+  "/api/all/claim/history/node",
+  historyClaimNodeController.getAllHistoryClaimNode
+);
+historyClaimNodeRouter.get(
+  "/api/claim/history/node/:_id",
+  historyClaimNodeController.getIdHistoryClaimNode
+);
+historyClaimNodeRouter.get(
+  "/api/claim/node/address/:_id",
+  historyClaimNodeController.getIdWalletAddress
+);
 module.exports = historyClaimNodeRouter;
